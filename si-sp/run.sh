99ae588a9f91a3518529f5a178d98940eb9eaff8
root <root@larissadocker.localdomain>
2017-06-14 13:40:47 +0200

    Default Apache configuration with copy command in run.sh

--- si-sp/run.sh
+++ si-sp/run.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 # Apache gets grumpy about PID files pre-existing
+
+if [ ! -f "/etc/ssl/localcerts/sp-cert.pem" ]; then
+echo "Service-Provider cetificate does not exist. I will create a self-signed ceritficate for you."
+openssl req -nodes -newkey rsa:2048 -keyout sp-certkey.key -out sp-cert.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=sp.com"
+fi
+
+if [  ! -f "/etc/apache2/sites-enabled/default-ssl.conf" ]; then
+echo "Default apache configuration does not exist. I will copy it for you."
+cp "/etc/apache2/sites-enabled/default-ssl.conf.example" "/etc/apache2/sites-enabled/default-ssl.conf"
+fi
+
 
 if [ -f "./run/apache2.pid" ]; then
 rm -f ./run/apache2.pid
@@ -8,3 +19,5 @@ fi
  service shibd start
  exec apache2 -DFOREGROUND
 #/usr/sbin/apache2ctl -D FOREGROUND
+
+
